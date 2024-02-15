96778b21be030d96d392487fc27d0cbd6a1c8073
Ramon <ramon.salaspi1.1@gmail.com>
2024-02-15 10:34:30 -0600

    update toggles & send_form

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ import parallax from "./components/parallax.js";
 import activeMenu from "./components/selected_menu.js";
 import updateCopyrightYear from "./helpers/date_updater.js";
 import resetToHome from "./helpers/reload_page.js";
+import sendEmail from "./helpers/send_form.js";
 
 
 parallax()
@@ -11,6 +12,11 @@ activeMenu()
 resetToHome()
 
 updateCopyrightYear()
+
+sendEmail()
+
+
+
 
 
 
